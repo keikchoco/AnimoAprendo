2ce2a077e0da31c8ecc0da0673e4d818d3487e00
JEREMIAH NUENO <NJH0090@dlsud.edu.ph>
2025-04-28 23:31:31 +0800

    updated navlinks

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer'
 import Header from '@/components/header'
+import NavLinks from '@/components/navlinks'
+import Link from 'next/link'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -36,25 +38,56 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased w-full min-h-screen m-auto flex flex-col`}>
-          <Header />
-          {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header> */}
           <SignedOut>
-          <div className='flex grow p-10 bg-green-950'>{children}</div>
+            <div className='flex grow p-10 bg-green-950'>{children}</div>
           </SignedOut>
           <SignedIn>
-            <div className='flex grow p-10'>{children}</div>
+            <div className="drawer grow">
+              <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+              <div className="drawer-content flex flex-col grow">
+                {/* Navbar */}
+                <div className="navbar bg-green-900 text-white w-full sticky top-0 z-999">
+                  <div className="flex-none lg:hidden">
+                    <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block h-6 w-6 stroke-current">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M4 6h16M4 12h16M4 18h16"></path>
+                      </svg>
+                    </label>
+                  </div>
+                  <div className="mx-2 flex-1 px-2">
+                    <div className="flex gap-5 items-center text-lg font-semibold">
+                      <Link href={"/"}>AnimoAprendo</Link>
+                    </div>
+                  </div>
+                  <div className="hidden flex-none lg:block h-10">
+                    <NavLinks />
+                  </div>
+                </div>
+                {/* Page content here */}
+                <div className="flex flex-col grow items-center gap-5 w-full pb-6">
+                  {children}
+                </div>
+              </div>
+              <div className="drawer-side">
+                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                  {/* Sidebar content here */}
+                  <NavLinks />
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <Footer />
+            </div>
           </SignedIn>
-          
-          
-          <Footer />
         </body>
       </html>
     </ClerkProvider>
