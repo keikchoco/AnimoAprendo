30908eaa95cda4f16bd246357be37582c357a65e
JEREMIAH NUENO <NJH0090@dlsud.edu.ph>
2025-05-07 14:32:13 +0800

    directory up

--- components/navlinkstutor.tsx
+++ components/navlinkstutor.tsx
@@ -18,7 +18,16 @@ async function NavLinksTutor() {
           <SwitchToTutee />
         </ul>
       </div>
+
       <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <Link className="btn btn-ghost btn-circle" href='/tutor/chat'>
+            <div className="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/><path d="M13 8H7"/><path d="M17 12H7"/></svg>
+              <span className="badge badge-md indicator-item font-bolder leading-0 align-top bg-none">1</span>
+            </div>
+          </Link>
+        </div>
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
@@ -37,11 +46,53 @@ async function NavLinksTutor() {
                 {/* <span className="badge">New</span> */}
               </Link>
             </li>
-
             <li><SignOutButton /></li>
           </ul>
         </div>
       </div>
+
+      {/* <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+              <div className="card-body">
+                <span className="text-lg font-bold">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">View cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div tabIndex={1} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src={user?.imageUrl} />
+            </div>
+          </div>
+          <ul
+            tabIndex={1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-black">
+            <li className='border-b'><a>Welcome, {user?.username}</a></li>
+            <li>
+              <Link className="justify-between" href="/tutor/profile">
+                Profile
+              </Link>
+            </li>
+
+            <li><SignOutButton /></li>
+          </ul>
+        </div>
+      </div> */}
     </div>
   )
 }
