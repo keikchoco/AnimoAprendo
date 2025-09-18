3e9bebba26af90e14815a8ee218a3f4c4a3bb5db
JEREMIAH NUENO <jeremiahnueno2017@gmail.com>
2025-09-18 14:29:45 +0800

    [API] clerkcreate

--- app/api/clerkcreate/route.ts
+++ app/api/clerkcreate/route.ts
@@ -1,4 +1,5 @@
 import clientPromise from "@/lib/mongodb";
+import { clerkClient } from "@clerk/nextjs/dist/types/server";
 import { stat } from "fs";
 import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
@@ -13,24 +14,37 @@ export async function POST(req: NextRequest) {
     // Parse the incoming JSON payload
     const data = await req.json();
 
-    if (data.data.type === "user.created") {
+    if (data.type === "user.created") {
+      const client = await clerkClient();
+      client.users.updateUserMetadata(data.data.id, { publicMetadata: {onboarded: false} });
       // Remove "user_" prefix from the id using regex if present
       const userId = data.data.id.replace(/^user_/, "");
 
       // Insert the user data into the "users" collection
-      const userData = await db.collection("users").insertOne({
+      const user = await db.collection("users").insertOne({
         _id: userId,
         ...data.data,
       });
 
+      const userData = await db.collection("userdata").insertOne({
+        userId: userId,
+        tutorData: {},
+        tuteeData: {},
+        createdAt: new Date().getTime(),
+        updatedAt: new Date().getTime(),
+      });
+
       // Log the event or process user creation
-      console.log("Received Clerk webhook:", userData);
+      console.log("Received Clerk webhook:", { user, userData });
 
       // Respond with 200 OK
       return NextResponse.json({ success: true, data }, { status: 200 });
     }
-    
-    return NextResponse.json({ success: false, message: "Not a user.created event" }, { status: 400 });
+
+    return NextResponse.json(
+      { success: false, message: "Not a user.created event" },
+      { status: 400 }
+    );
   } catch (error) {
     console.error("Error handling Clerk webhook:", error);
     return NextResponse.json({ error }, { status: 400 });
